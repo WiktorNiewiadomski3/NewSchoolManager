35d532c9b96a0a7a59b6cb0f7658ea05da2f6676
Wiktor Niewiadomski <wiktor.niewiadomski3@gmail.com>
2021-02-16 16:19:49 +0100

    Added EF Core and Models

---
+++ SchoolManager/SchoolManager/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolManager.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
